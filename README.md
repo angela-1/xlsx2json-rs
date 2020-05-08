3d7c24450c74a5dce3a5a44a4c5ba504e46a0d54
angela-1 <angela-1@users.noreply.github.com>
2020-05-08 09:36:45 +0800

    add usage

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # xlsx2json-rs
-xlsx2json with Rust language
+A command line tool for parse `xlsx` file to `json` file in pure Rust.
+
+## Usage
+
+```
+USAGE:
+    xlsx2json.exe [OPTIONS] <XLSX_FILE>
+
+FLAGS:
+    -h, --help       Prints help information
+    -V, --version    Prints version information
+
+OPTIONS:
+    -o, --output <JSON_FILE>    Output json file
+    -r, --row <NUMBER>          Row number of key, zero index start, default 0
+
+ARGS:
+    <XLSX_FILE>    Input xlsx file
+```
+
+
--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,9 @@ use clap::{App, Arg};
 use std::collections::HashMap;
 use std::fs;
 
+/**
+ * 获取xlsx文件的标题行作为json对象的键值
+ */
 fn get_keys(start: u32, range: &Range<DataType>) -> Vec<String> {
     let col = range.get_size().1 as u32;
     let mut keys: Vec<String> = vec![];
@@ -13,15 +16,21 @@ fn get_keys(start: u32, range: &Range<DataType>) -> Vec<String> {
     keys
 }
 
+/**
+ * 获取xlsx文件数据范围
+ */
 fn get_range(path: &str) -> Result<Range<DataType>, Error> {
     let mut workbook: Xlsx<_> = open_workbook(path)?;
     let sheet_name = workbook.sheet_names()[0].clone();
     let range = workbook
         .worksheet_range(&sheet_name)
-        .ok_or(Error::Msg("Cannot read first sheet."))??;
+        .ok_or(Error::Msg("Cannot read first sheet"))??;
     Ok(range)
 }
 
+/**
+ * 解析一行生成一个hashmap对象
+ */
 fn parse_row(keys: &Vec<String>, range: &Range<DataType>) -> HashMap<String, String> {
     let mut line: HashMap<String, String> = HashMap::new();
     for n in 0..range.get_size().1 {
@@ -30,53 +39,69 @@ fn parse_row(keys: &Vec<String>, range: &Range<DataType>) -> HashMap<String, Str
     line
 }
 
-fn main() {
-    let matches = App::new("xlsx2json")
-        .version("0.1")
-        .author("Angela-1 <mail>")
-        .about("xlsx2json can generate json file from xlsx file with title as keys.")
+/**
+ * 转换函数
+ */
+fn transform() {
+    let arguments = App::new("xlsx2json")
+        .version("0.0.2")
+        .author("Angela-1 <ruoshui_engr@163.com>")
+        .about("A command line tool for parse xlsx file to json file.")
         .arg(
-            Arg::with_name("INPUT")
-                .help("Sets the input file to use")
+            Arg::with_name("input")
+                .value_name("XLSX_FILE")
+                .help("Input xlsx file")
                 .required(true)
-                .index(1),
+                .takes_value(true),
         )
         .arg(
-            Arg::with_name("OUTPUT")
-                .help("Sets the output file to use")
-                .index(2)
+            Arg::with_name("output")
+                .short("o")
+                .long("output")
+                .value_name("JSON_FILE")
+                .help("Output json file"),
         )
         .arg(
-            Arg::with_name("start")
-                .short("s")
-                .long("start")
+            Arg::with_name("row")
+                .short("r")
+                .long("row")
                 .value_name("NUMBER")
-                .help("Sets title line number"),
+                .help("Row number of key, zero index start, default 0"),
         )
-        
         .get_matches();
 
-    let file = matches
-        .value_of("INPUT")
+    let file = arguments
+        .value_of("input")
         .expect("Please provide a xlsx file");
 
-    let title_line = matches.value_of("start").unwrap_or("0").parse().unwrap();
+    // 获取键值的行号
+    let key_row: u32 = arguments.value_of("row").unwrap_or("0").parse().unwrap();
 
+    // 默认输出json文件名
     let default_result_filename = file.to_string() + ".json";
-    let dest = matches.value_of("OUTPUT").unwrap_or(&default_result_filename);
+    let dest = arguments
+        .value_of("output")
+        .unwrap_or(&default_result_filename);
 
-    let range = get_range(&file).ok().expect("fail to get range.");
-    let keys = get_keys(title_line, &range);
+    let range = get_range(&file).ok().expect("fail to get range");
+    // 获取json对象键值
+    let keys = get_keys(key_row, &range);
 
-    let mut result: Vec<HashMap<String, String>> = Vec::new();
+    let mut key_value_map: Vec<HashMap<String, String>> = Vec::new();
 
-    let (row, col) = range.get_size();
-    let data_line = title_line + 1;
-    for n in data_line..row as u32 {
-        let r = range.range((n, 0), (n, col as u32 - 1));
-        result.push(parse_row(&keys, &r));
+    let (row, col): (usize, usize) = range.get_size();
+    let data_row = key_row + 1;
+    for n in data_row..row as u32 {
+        let rec = range.range((n, 0), (n, col as u32 - 1));
+        key_value_map.push(parse_row(&keys, &rec));
     }
-    let s = serde_json::ser::to_string(&result).unwrap();
-    fs::write(dest, s).expect("Unable to write file");
+
+    // 将hashmap转换为带格式的json字符串
+    let json_string = serde_json::to_string_pretty(&key_value_map).unwrap();
+    fs::write(dest, json_string).expect("Unable to write file");
     println!("Done");
+}
+
+fn main() {
+    transform();
 }
